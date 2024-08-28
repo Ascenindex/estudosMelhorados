6c4f65a29407849c1f0a53eaafe3105b66011c6a
LippiDev <132282100+Ascenindex@users.noreply.github.com>
2024-08-28 20:19:36 -0300

    continuando aprendendo arrays

---
+++ src/com/estudosJava/Introducao/Aula07Arrays01.java
@@ -0,0 +1,12 @@
+package com.estudosJava.Introducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        idades[0] = 21;
+        idades[1] = 12;
+        idades[2] = 54;
+        System.out.println(idades[0]);
+
+    }
+}
---
+++ src/com/estudosJava/Introducao/Aula07Arrays02.java
@@ -0,0 +1,7 @@
+package com.estudosJava.Introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+
+    }
+}
